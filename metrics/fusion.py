768ea818b96e0ab8b1dd7d82c93bcfb396f05d43
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-10-23 00:07:10 +0200

    feat: first msi2slstr_loss implementation
    closes #6

---
+++ metrics/fusion.py
@@ -0,0 +1,26 @@
+from .ssim import ssim
+from torch import Tensor
+from torch import concat
+
+from transformations.resampling import DSamplingModule
+
+
+class msi2slstr_loss(ssim):
+    """
+    A loss definition for the task of fusing S2 L1C & S3 RBT/LST images.
+
+    Components: ...
+    """
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+
+    def forward(self, x: Tensor, Y_hat: Tensor, y: Tensor,
+                thermal_estimate_y: Tensor, thermal_estimate_x: Tensor):
+        """
+        """
+        x = concat([x[:, [2, 3, 8, 10, 11, 12]], thermal_estimate_x], dim=-3)
+        structure = self.s(x, Y_hat)
+        # Scalar.
+        thermal = super().forward(thermal_estimate_y, y[:, 6:]).mean()
+        energy = super().forward(y, DSamplingModule(Y_hat))
+        return sum([energy, structure, thermal]), energy, thermal

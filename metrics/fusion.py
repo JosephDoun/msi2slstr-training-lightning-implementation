723df60057699a81897bfabf78b3d55b0daf6526
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-10-25 00:09:00 +0200

    fix: changed `sum` aggregation to `mean`

--- metrics/fusion.py
+++ metrics/fusion.py
@@ -15,6 +15,10 @@ class msi2slstr_loss(ssim):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
+    @staticmethod
+    def _dsample(x: Tensor):
+        return DSamplingModule(x)
+
     def forward(self, x: Tensor, Y_hat: Tensor, y: Tensor,
                 thermal_estimate_y: Tensor, thermal_estimate_x: Tensor):
         """
@@ -25,5 +29,8 @@ class msi2slstr_loss(ssim):
         structure = self.s(x, Y_hat)
         # Scalar.
         thermal = super().forward(thermal_estimate_y, y[:, 6:]).mean()
-        energy = super().forward(y, DSamplingModule(Y_hat))
-        return sum([energy, structure, thermal]), energy, thermal
+        energy = super().forward(y, self._dsample(Y_hat))
+        return sum([energy, structure, thermal]).div(3.)
+
+    def energy(self, x: Tensor, y: Tensor) -> Tensor:
+        return super().evaluate(x, DSamplingModule(y))

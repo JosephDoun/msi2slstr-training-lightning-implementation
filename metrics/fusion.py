2dce7669fb66431a00a281b75fd934d4a4167dff
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-10-28 02:29:33 +0100

    feat(metrics): added x input masking on fusion loss

--- metrics/fusion.py
+++ metrics/fusion.py
@@ -1,7 +1,8 @@
 from torch import Tensor
 from torch import concat
 
-from transformations.resampling import DSamplingModule
+from transformations.resampling import AvgDownSamplingModule
+from transformations.resampling import UpsamplingModule
 
 from .ssim import ssim
 
@@ -15,9 +16,11 @@ class msi2slstr_loss(ssim):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-    @staticmethod
-    def _dsample(x: Tensor):
-        return DSamplingModule(x)
+    def _dsample(self, x: Tensor):
+        return AvgDownSamplingModule(x)
+    
+    def _mask_x(self, high_x: Tensor, low_y: Tensor):
+        return high_x.mul(UpsamplingModule(low_y).gt(0))
 
     def forward(self, x: Tensor, Y_hat: Tensor, y: Tensor,
                 thermal_estimate_y: Tensor, thermal_estimate_x: Tensor):
@@ -25,12 +28,17 @@ class msi2slstr_loss(ssim):
         :returns: Collective loss, energy loss and thermal loss in a 3-tuple.
         :rtype: tuple
         """
+        # zero y pixels must be masked out of x for consistency.
         x = concat([x[:, [2, 3, 8, 10, 11, 12]], thermal_estimate_x], dim=-3)
+        # Mask out irrelevant pixels using LST band.
+        x = self._mask_x(x, y[:, [-1]])
+        # Not respecting zero pixels pf y.
         structure = self.s(x, Y_hat)
         # Scalar.
         thermal = super().forward(thermal_estimate_y, y[:, 6:]).mean()
+        # Respecting zero pixels of y but not of x. Not important.
         energy = super().forward(y, self._dsample(Y_hat))
         return sum([energy, structure, thermal]).div(3.)
 
     def energy(self, x: Tensor, y: Tensor) -> Tensor:
-        return super().evaluate(x, DSamplingModule(y))
+        return super().evaluate(x, AvgDownSamplingModule(y))

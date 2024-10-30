ea87cfa245ef3c26521f7da9c3e24a23d621c7ea
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-10-30 18:11:33 +0100

    style: added F401 exceptions to main script

--- msi2slstr-training.py
+++ msi2slstr-training.py
@@ -1,6 +1,6 @@
 from lightning.pytorch.cli import LightningCLI
-from datamodules.lightning import msi2slstr_datamodule # noqa: 
-from modules.lightning import msi2slstr # noqa: 
+from datamodules.lightning import msi2slstr_datamodule # noqa: F401
+from modules.lightning import msi2slstr # noqa: F401
 
 import sys
 

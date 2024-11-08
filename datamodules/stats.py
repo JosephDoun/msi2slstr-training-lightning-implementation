6c62bc6013f685378b4c8835c9df35dade211a2d
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-11-08 22:36:35 +0100

    refactor: moved statistics gathering workflow to scripts

--- datamodules/stats.py
+++ datamodules/stats.py
@@ -1,10 +1,11 @@
 from torch import Tensor
-from tqdm import tqdm
-
 from typing import Any
 
 
 class Average(Tensor):
+    """
+    ...
+    """
 
     def __init__(self, *args, **kwargs):
         self.count = 1
@@ -39,39 +40,3 @@ class Average(Tensor):
 
     def __matmul__(self, other: Any) -> Tensor:
         raise NotImplementedError()
-
-
-def write_stats(args):
-    from config import DATA_CONFIG
-    from config import put_yaml_dict
-
-    def load_stats(dataset, name):
-        sample = dataset[0]
-        avg = Average(sample.mean((-1, -2)))
-        var = Average(sample.var((-1, -2)))
-        bar = tqdm(dataset)
-        for sample in bar:
-            avg = avg + sample.mean((-1, -2))
-            var = var + sample.var((-1, -2))
-        
-        stats = DATA_CONFIG.get("stats", {})
-        stats.update({name: {"mean": avg.tolist(), "var": var.tolist()}})
-        DATA_CONFIG.update(stats)
-
-    if "sen3" in args:
-        from datasets import sen3dataset
-        load_stats(sen3dataset(210), "sen3")
-
-    if "sen2" in args:
-        from datasets import sen2dataset
-        load_stats(sen2dataset(5250), "sen2")
-    
-    # Write.
-    put_yaml_dict(DATA_CONFIG, "../config/data.yaml")
-
-
-if __name__ == "__main__":
-    import sys
-    sys.path.append(".")
-
-    write_stats(sys.argv)

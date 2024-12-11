321eaca6daca8fc8c5e395f3f67ed5560c406493
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2024-12-11 21:30:27 +0100

    feat: updated msi2slstr_function

--- metrics/fusion.py
+++ metrics/fusion.py
@@ -17,6 +17,8 @@ class msi2slstr_loss(ssim):
     """
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        #: Channel axis ssim to evaluate
+        #  pixel spectral signatures.
         self.signature = ssim((-3,))
 
     def _dsample(self, x: Tensor):
@@ -41,32 +43,34 @@ class msi2slstr_loss(ssim):
         # Mask out irrelevant pixels using the green band.
         x = self._mask_x(x, y[:, [0]])
 
-        # Calculate scene maximum.
-        scenemax = x.amax((-1, -2), keepdim=True)
-
-        # Scale scene.
-        composition = x.div(scenemax + 1e-5)
-
-        # Calculate scene mean.
-        scenemean = composition.mean((-1, -2), keepdim=True)
+        # Scale scene to max 1.
+        topology = x.div(x.amax((-1, -2), keepdim=True) + 1e-5)
 
         # Offset scene to have mean 1.
-        composition = composition - scenemean + 1
+        topology = topology - topology.mean((-1, -2), keepdim=True) +1
 
-        # Map w/ adjusted mean and relative variance.
-        fusion_map = composition * y.mean((-1, -2), keepdim=True)
+        # Map w/ adjusted mean and accordingly scaled variance.
+        # Scale each channel to have the mean of y.
+        # Can also try pixel (y) based mean adjustment. TODO.
+        topology = topology * self._usample(y) # .mean((-1, -2), keepdim=True)
 
-        structure = self.s(Y_hat, fusion_map).mul(
+        # Balance between high-res correlation
+        # and low-res decorrelation conditions.
+        topology = self.s(Y_hat, topology).mul(
 
-            # Explicitly remove plane flatness.            
-            (1 - self.s(Y_hat, self._usample(y)))
+            # Explicitly resist plane flatness.
+            # (Maintain subpixel variance as high
+            #  as permitted by high-res correlation.)
+            (1 - self.s(Y_hat, self._usample(y))) ** 2
 
             )\
+                .mul(self.c(Y_hat, topology) ** .2)\
                 .clamp(.0)
 
         thermal = super().forward(thermal_estimate_y, y[:, 6:])
 
         thermal = thermal.add(
+            # Include channel-axis pixel signature similarity.
             self.signature(thermal_estimate_y, y[:, 6:])
             .clamp(0)
             .mean((-1, -2))
@@ -80,8 +84,8 @@ class msi2slstr_loss(ssim):
             .mean((-1, -2))\
             .unsqueeze(1)\
 
-        return energy.mul(signature).mul(structure), thermal
-    
+        return energy.mul(signature).mul(topology), thermal
+
     def __call__(self, x: Tensor, Y_hat: Tensor, y: Tensor,
                  thermal_estimate_y: Tensor,
                  thermal_estimate_x: Tensor) -> Tensor:

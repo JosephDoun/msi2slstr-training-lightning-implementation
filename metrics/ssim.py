cd688ffc32555c959263027811e86c7e78675624
Joseph Doundoulakis <iosif.doundoulakis@outlook.com>
2025-02-25 04:31:55 +0100

    fix: ssim loss corrections

--- metrics/ssim.py
+++ metrics/ssim.py
@@ -2,6 +2,8 @@ from torch.nn import Module
 from torch import Tensor
 from torch import stack
 
+import torch
+
 
 class ssim(Module):
     """
@@ -34,26 +36,23 @@ class ssim(Module):
             a.mul(b).mul(2).add(self.C)
             # Denominator.
             .div(
-                a.pow(2).add(b.pow(2))
+                a.pow(2)
+                .add(b.pow(2))
                 .add(self.C)
                 )
             .squeeze(self.dims))
 
     def l(self, x: Tensor, y: Tensor) -> Tensor:
-        # Move mean ranges to the positives.
         xmean = x.mean(self.dims, keepdim=True)
         ymean = y.mean(self.dims, keepdim=True)
-        # NOTE: Can use adequate constant to avoid min calculation.
-        _min = xmean.minimum(ymean).detach_() - .1
-        return self._similarity(xmean - _min,
-                                ymean - _min)\
-                                .pow(self._a)
+        meanmin = xmean.detach().minimum(ymean.detach()).sub(.1)
+        return self._similarity(xmean - meanmin,
+                                ymean - meanmin).pow(self._a)
 
     def c(self, x: Tensor, y: Tensor) -> Tensor:
         return self._similarity(x.std(self.dims, keepdim=True),
-                                y.std(self.dims, keepdim=True))\
-                                .pow(self._b)
-    
+                                y.std(self.dims, keepdim=True)).pow(self._b)
+
     def s(self, x: Tensor, y: Tensor) -> Tensor:
         xstd = x.std(self.dims, keepdim=True)
         ystd = y.std(self.dims, keepdim=True)
@@ -86,11 +85,10 @@ class ssim(Module):
         Averaged structural similarity loss with a range of values of
         [-1, 1] for maximization.
         """
-        return getattr(stack([self.l(x, y), self.c(x, y), self.s(x, y)]),
-                       self._agg)(0)
+        return stack([self.l(x, y), self.c(x, y), self.s(x, y)]).prod(0)
 
 
-class cubic_ssim(ssim):
+class ssim3d(ssim):
     """
     Includes the channel axis in the structural similarity measurement.
     """
@@ -98,6 +96,7 @@ class cubic_ssim(ssim):
                  b: float = 1, c: float = 1) -> None:
         super().__init__((-1, -2), agg=agg, a=a, b=b, c=c)
         self._channel_axis = ssim((-3,), agg=agg, a=a, b=b, c=c)
+        self._aggregate = getattr(torch, agg)
 
     def forward(self, x: Tensor, y: Tensor) -> Tensor:
         channel_axis = self._channel_axis(x, y).mean((-1, -2)).unsqueeze(-1)
